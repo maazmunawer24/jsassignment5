a58511df7d0b1115d3f1901c633dca06e8d84d39
maazmunawer24 <maaz.munawer@outlook.com>
2020-06-18 23:21:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+//  WM-5047
+//     maaz munawer farooqui
+
+///////////////////chapter 1
+
+//task 1
+// alert('welcome to our website');
+
+//task2
+// alert('Error! Please engter a valid password');
+
+//task3
+// alert("welcome to js land... \n Happy coding!");
+
+//task4
+// alert("Welcome to js land");
+// alert("Happy coding ");
+
+//task 5
+// console.log("Hello..I can run JS through my browser's console");
+
+//task 6
+// var input = prompt("enter your country");
+
+// if(input === 'Pakistan')
+// {
+//     alert("proceed");
+// }
+// else{
+//     alert("access denied");
+// }
+//task 7
+//completed in index.html file, tried script tag at different positions.
+////////////////////chapter 2
+//task 1
+// var username;
+
+//task 2
+// var myname = "maaz munawer";
+
+//task 3
+// var message;
+// message  = "helloworld";
+// alert(message);
+
+//task 4
+// var name =prompt("enter your name");
+// var age = prompt("enter your age");
+// var gender = prompt("enter your gender");
+
+// alert("your name is : "+name+"\n your age is : "+age+"\n your gender is "+gender)
+
+//task 5
+//  var value = "Pizza";
+// for (var i=5; i>0; i--){
+//     alert(value.slice(0,i));
+// }
+
+//task 6
+// var email = "maaz.munawer@outlook.com"
+
+// alert("my address is "+email)
+
+//task 7
+// var book = "A smarter way to learn JavaScript";
+
+// alert("I am trying to learn from the book."+book);
+
+//task 8
+//document.write("yah! I can write in htm using js")
